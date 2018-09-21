4c23484bfa2da62d42e66b5e4d2af15c2dbd3ab9
cfarinello <cfarinello@gmail.com>
2018-09-21 10:35:38 -0300

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,14 +1,14 @@
  node('maven') {
     stage('build') {
         echo 'building app :)'
-        openshiftBuild(buildConfig: 'workshop-openshift', showBuildLogs: 'true')
+        openshiftBuild(buildConfig: 'php-teste', showBuildLogs: 'true')
     }
     stage('verify') {
         echo 'dummy verification....'
     }
     stage('deploy') {
         input 'Manual Approval'
-        openshiftDeploy(deploymentConfig: 'workshop-openshift')
+        openshiftDeploy(deploymentConfig: 'php-teste')
     }
     stage('promoting to QA') {
        echo 'fake stage...'
